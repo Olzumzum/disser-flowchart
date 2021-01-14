7ed09d1a102036c9afad6083e524f2c7db8cd6d9
Olzumzum <evdokimova_olga_15@mail.ru>
2021-01-14 17:30:39 +0700

    created pages

---
+++ src/pages/File_s.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export default class File_s extends Component{
+    render() {
+        return(
+            <div>
+                Saved file
+            </div>
+        )
+    }
+
+}
