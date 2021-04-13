5723c265477c9f08f2ddb3cc96411f303e130bfa
John Connor <sashavohmin@gmail.com>
2021-04-13 14:08:01 +0700

    Попытка добавить onClick

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -7,7 +7,7 @@ export default class Toolbar extends Component {
             <Container fluid>
                 <div className={"p-3 mb-2 bg-secondary"} >
                     <Button className={"mr-2"} size={"sm"}>Кнопка назад</Button>
-                    <Button className={"mr-2"} size={"sm"}>Кнопка вперед</Button>
+                    <Button onClick={foo}  className={"mr-2"} size={"sm"}>Кнопка вперед</Button>
                     <Button className={"mr-2"} size={"sm"}>Сохранить</Button>
                     <Button className={"mr-2"} size={"sm"}>Запуск</Button>
                     <Button className={"mr-2"} size={"sm"}>Отладка</Button>
@@ -16,4 +16,7 @@ export default class Toolbar extends Component {
         );
     }
 
+}
+function foo(){
+    alert('hi');
 }
