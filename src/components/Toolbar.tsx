ea60d6eb4a8ed90f6cea1a4f7d33dc8a7233a9e6
John Connor <sashavohmin@gmail.com>
2021-04-15 04:01:54 +0700

    Проверка отката + функция на кнопку

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,35 +1,32 @@
 import React, {Component} from "react";
 import {Button, Container} from "react-bootstrap";
-<script src="~/myscript.js"></script>
+import {load_file} from './compilator/compilator.js'
+import {read_file} from './compilator/compilator.js'
+
 export default class Toolbar extends Component {
     render() {
         return (
             <Container fluid>
                 <div className={"p-3 mb-2 bg-secondary"} >
-                    <form action="compilator.php" >
-                        <Button className={"mr-2"} size={"sm"}>Кнопка назад</Button>
-                        <Button className={"mr-2"} size={"sm"}>Кнопка вперед</Button>
-                        <Button className={"mr-2"} size={"sm"}>Сохранить</Button>
-                        <Button className={"mr-2"} size={"sm"}>Запуск</Button>
-                        <Button className={"mr-2"} size={"sm"}>Отладка</Button>
-                        <Button className={"mr-2"} size={"sm"}> <input type="file" name="file" /></Button>
-                        <Button className={"mr-2"} size={"sm"}>
-                            <select>
-                                <option value = "Java">Java</option>
-                                <option value = "Python">Python</option>
-                                <option value = "C++">C++</option>
-                                <option value = "JavaScript">JavaScript</option>
-                            </select>
-                        </Button>
-                        <Button className={"mr-2"} size={"sm"}><input type="submit" name="sub" value="Построить" /></Button>
-                    </form>
+                    <Button className={"mr-2"} size={"sm"}>Кнопка назад</Button>
+                    <Button className={"mr-2"} size={"sm"}>Кнопка вперед</Button>
+                    <Button className={"mr-2"} size={"sm"}>Сохранить</Button>
+                    <Button className={"mr-2"} size={"sm"}>Запуск</Button>
+                    <Button className={"mr-2"} size={"sm"}>Отладка</Button>
+                    <Button className={"mr-2"} size={"sm"}>
+                        <select>
+                            <option value = "Java">Java</option>
+                            <option value = "Python">Python</option>
+                            <option value = "C++">C++</option>
+                            <option value = "JavaScript">JavaScript</option>
+                        </select>
+                    </Button>
+                    <Button onClick={load_file} className={"mr-2"} size={"sm"}>Построить
+                        <input id="uploaded_file" type="file" name="file_upl" onChange={read_file} hidden />
+                    </Button>
                 </div>
             </Container>
 
         );
-        function test(){
-            alert("кайф");
-        }
     }
-
 }
