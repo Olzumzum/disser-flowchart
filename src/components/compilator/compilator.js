cdf8756cf34c06cc244c12d4c5c6e569730b27ed
John Connor <sashavohmin@gmail.com>
2021-04-15 03:12:16 +0700

    Борюсь с откатом мастера

--- src/components/compilator/compilator.js
+++ src/components/compilator/compilator.js
@@ -1,3 +1,26 @@
-function test(){
-    alert("dwqdw");
+//Загрузка файла пользователя
+export function load_file(){
+    document.getElementById("uploaded_file").click();
+}
+
+//Чтение файла пользователя
+export function read_file(){
+    debugger
+    let text = "";
+    let file = document.getElementById("uploaded_file").files[0];
+    let file_name = file.name;
+    let reader = new FileReader();
+    reader.addEventListener('load', function (e){
+        text = e.target.result;
+        compile_in(text);
+    });
+    reader.addEventListener('error', function() {
+        alert('Error : Failed to read file');
+    });
+    reader.readAsText(file);
+}
+
+// Обработка выбранного файла - преобразование во внутренний формат
+function compile_in(text){
+    alert(text);
 }
