c6f07364dee4c29f1d13466eed120511ca2cc65c
Olzumzum <program.olga@gmail.com>
2021-04-20 21:10:13 +0700

    Пакет адаптера

---
+++ src/components/adapter/DisplayCompilerAdapter.ts
@@ -0,0 +1,15 @@
+
+//преобразование данных от компилятора в отображение
+export const compilerToDispay = (data: any) => {
+    let result: any[] | null = null
+
+    //возвращать будет другое, пока неизвестно что
+    return result
+}
+
+//преобразование данных от интерфейса в данные для компилятора
+export const displayToCompiler = (data: any) => {
+    let result: any[] | null = null
+
+    return result
+}
